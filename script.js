f808223683ba66a9b7337d156ac1c20d14eb9d86
pritam dutta <webdevpritam7498@gmail.com>
2024-05-23 23:17:53 +0600

    creating a function for use the compute move

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('project init')
+// function for computerMoves
+
+function computerMoves(computerMove) {
+  let randomNum = Math.random();
+  // console.log(randomNum)
+  if (randomNum > 0 && randomNum < 1 / 3) {
+    computerMove = "rock";
+    console.log(computerMove);
+  } else if (randomNum > 1 / 3 && randomNum < 2 / 3) {
+    computerMove = "paper";
+    console.log(computerMove);
+  } else if (randomNum > 2 / 3 && randomNum < 1) {
+    computerMove = "scissor";
+    console.log(computerMove);
+  }
+  
+  return computerMove;
+}
