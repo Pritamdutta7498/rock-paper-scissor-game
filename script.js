be389b9ea2bb3433663d253c0bed9ac2b275a8f9
pritam dutta <webdevpritam7498@gmail.com>
2024-05-24 10:46:41 +0600

    using all condition and console the computeMove, playerMove and the result

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 let result = "";
 function playerMoves(playerMove) {
-  let computerMove = computerMoves(); 
+  let computerMove = computerMoves();
   if (playerMove === "rock") {
-
-    // console.log('player move is rock')
-
     if (computerMove === "rock") {
       result = "Tie";
     } else if (computerMove === "paper") {
@@ -12,10 +9,27 @@ function playerMoves(playerMove) {
     } else if (computerMove === "scissor") {
       result = "Win";
     }
-    console.log("result is: you", result);
   } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "Win";
+    } else if (computerMove === "paper") {
+      result = "Tie";
+    } else if (computerMove === "scissor") {
+      result = "Loss";
+    }
   } else if (playerMove === "scissor") {
+    if (computerMove === "rock") {
+      result = "Loss";
+    } else if (computerMove === "paper") {
+      result = "Win";
+    } else if (computerMove === "scissor") {
+      result = "Tie";
+    }
   }
+  console.log(`your Move: ${playerMove} | computer Move: ${computerMove} 
+  \nresult is: ${result}
+  `);
+
   return playerMove;
 }
 
@@ -27,13 +41,10 @@ function computerMoves() {
   // console.log(randomNum)
   if (randomNum > 0 && randomNum < 1 / 3) {
     computerMove = "rock";
-    console.log(computerMove);
   } else if (randomNum > 1 / 3 && randomNum < 2 / 3) {
     computerMove = "paper";
-    console.log(computerMove);
   } else if (randomNum > 2 / 3 && randomNum < 1) {
     computerMove = "scissor";
-    console.log(computerMove);
   }
 
   return computerMove;
