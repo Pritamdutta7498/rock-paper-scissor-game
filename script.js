6465c37081f76345bce041326b7b4debf57f5ecf
pritam dutta <webdevpritam7498@gmail.com>
2024-05-24 10:59:04 +0600

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let result = "";
-function playerMoves(playerMove) {
+function playGames(playerMove) {
   let computerMove = computerMoves();
   if (playerMove === "rock") {
     if (computerMove === "rock") {
@@ -29,8 +29,6 @@ function playerMoves(playerMove) {
   console.log(`your Move: ${playerMove} | computer Move: ${computerMove} 
   \nresult is: ${result}
   `);
-
-  return playerMove;
 }
 
 // function for computerMoves
@@ -49,3 +47,15 @@ function computerMoves() {
 
   return computerMove;
 }
+
+/* 
+step-1: making random nunmber for computer move and the algo is
+          0 - 1/3 = rock
+          1/3- 2/3 = paper
+          2/3 - 1/3 = scissor
+step-2: making a function where we have to return the computer move for
+        compare with player move;
+step-3: making a game function where we have to make condition for player
+        move and compare with computer move, and call it with button;
+
+          */
