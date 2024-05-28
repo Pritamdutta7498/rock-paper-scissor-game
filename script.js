e6a28fb8fbcc7a27551564ab1573f20295b8d3f8
pritam dutta <webdevpritam7498@gmail.com>
2024-05-29 03:10:58 +0500

    update

--- script.js
+++ script.js
@@ -77,16 +77,3 @@ function computerMoves() {
   return computerMove;
 }
 
-/* 
-step-1: making random nunmber for computer move and the algo is
-        0 - 1/3 = rock
-        1/3- 2/3 = paper
-        2/3 - 1/3 = scissor
-step-2: making a function where we have to return the computer move for compare with player move;
-step-3: making a game function where we have to make condition for player move and compare with computer move,
-        and call it with button;
-step-4: setup the scores how many time win and loss, and the updateScores function is setup for update
-         and set the scores to localStorage and get data from there.
-setp-4: implement the reset button and design the UI.
-
-*/
